440a635684b4243fa9b5c6d94b3fe499cf0a3127
Yury Nino <yury.nino.roa@gmail.com>
2020-06-22 10:51:45 -0500

    :sparkles: workshop - step 3

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "consul_workspace" {
   type        = string
+  default     = "yury-r-consul"
   description = "Workspace that created the Consul cluster"
 }
 
 variable "cluster_workspace" {
   type        = string
+  default     = "yury-r-k8s"
   description = "Workspace that created the Kubernetes cluster"
 }
 
 variable "organization" {
   type        = string
+  default     = "infrastructure-pipelines-workshop"
   description = "Organization of workspace that created the Kubernetes cluster"
 }
